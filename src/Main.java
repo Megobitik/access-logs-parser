af13c97d3997e33f6b9ff36f0db825627ec49af4
Alex <ALagutenko@inno.tech>
2022-11-22 16:29:27 +0300

    fix out numbers

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите число");
+        System.out.println("Введите первое число");
         int number = new Scanner(System.in).nextInt();
-    }
+        System.out.println("Введите второе число");
+        int numberTwo = new Scanner(System.in).nextInt();
+
+        int firstNumber = 5;
+        int secondNumber = 10;
+
+        double quotient = (double) firstNumber / secondNumber;
+            System.out.println("Частное чисел " +quotient);
+        int summ = (int) firstNumber + secondNumber;
+            System.out.println("Сумма чисел " +summ);
+        int minus = (int) firstNumber - secondNumber;
+            System.out.println("Разность чисел " +minus);
+        int multiplication = (int) firstNumber * secondNumber;
+            System.out.println("Произведение чисел " + multiplication);
+        }
 }
