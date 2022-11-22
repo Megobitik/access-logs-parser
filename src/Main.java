6d1952cabcd194933f0e957c6e6507cbd2a98734
Alex <ALagutenko@inno.tech>
2022-11-22 16:13:45 +0300

    added conclusion integer

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " +
-                Math.random());
+        System.out.println("Введите число");
+        int number = new Scanner(System.in).nextInt();
     }
 }
